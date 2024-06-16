f05fe1e37fafdc20032a2bedce2502b14d8ffcce
JoseMM2002 <jeolmmt@gmail.com>
2024-06-16 15:22:21 -0600

    adding input

--- src/components/ui/globalComponents.ts
+++ src/components/ui/globalComponents.ts
@@ -1,8 +1,10 @@
 import type { App } from "vue";
 import { Button } from "./button";
 import { Icon } from "./icon";
+import { Input } from "./input";
 
 export const registerComponents = (app: App) => {
   app.component("PBtn", Button);
   app.component("PIcon", Icon);
+  app.component("PInput", Input);
 };
