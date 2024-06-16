6946219f492331542200c9e2d9dafa2c2329b3ed
JoseMM2002 <jeolmmt@gmail.com>
2024-06-16 15:18:18 -0600

    loginView

---
+++ src/views/LoginView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import TutorLogo from "../assets/Tutor_link_logo.png";
+</script>
+<template>
+  <main class="flex flex-row h-screen">
+    <div class="flex-1 flex flex-col justify-center">
+      <div class="w-full h-full px-32 flex flex-col justify-center bg-teal-950">
+        <img
+          :src="TutorLogo"
+          alt="Tutor Link Logo"
+          class="bg-white"
+        />
+      </div>
+    </div>
+    <div class="flex-1"></div>
+  </main>
+</template>
