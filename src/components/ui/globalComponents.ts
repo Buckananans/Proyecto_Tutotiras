7a575bd4e4ec77506f08024d0a70985c5ec5bb5d
JoseMM2002 <jeolmmt@gmail.com>
2024-06-24 23:05:57 -0600

    adding form

--- src/components/ui/globalComponents.ts
+++ src/components/ui/globalComponents.ts
@@ -2,9 +2,23 @@ import type { App } from "vue";
 import { Button } from "./button";
 import { Icon } from "./icon";
 import { Input } from "./input";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "./form";
 
 export const registerComponents = (app: App) => {
   app.component("PBtn", Button);
   app.component("PIcon", Icon);
   app.component("PInput", Input);
+  app.component("PFormField", FormField);
+  app.component("PFormLabel", FormLabel);
+  app.component("PFormControl", FormControl);
+  app.component("PFormMessage", FormMessage);
+  app.component("PFormDescription", FormDescription);
+  app.component("PFormItem", FormItem);
 };
